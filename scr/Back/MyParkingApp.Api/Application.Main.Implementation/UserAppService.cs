bb306748f9dc5940877dca0d608a5c4205f7c7b6
SURFACE-JEY\jeyss <jeysson.ramirez@banlinea.com>
2018-10-24 16:54:33 -0500

    using repository

--- scr/Back/MyParkingApp.Api/Application.Main.Implementation/UserAppService.cs
+++ scr/Back/MyParkingApp.Api/Application.Main.Implementation/UserAppService.cs
@@ -2,14 +2,35 @@
 using Application.Main.Definition;
 using Core.DataTransferObject;
 using Core.Entities;
+using Core.GlobalRepository;
 
 namespace Application.Main.Implementation
 {
     public class UserAppService : IUserAppService
     {
+        private readonly IUserRepository _userRepository;
+
+        public UserAppService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
         public BaseApiResponse RegisterUser(User data)
         {
-            return  new BaseApiResponse{Message = "Esta Vivo jejeje :)"};
+            var response = new BaseApiResponse();
+            try
+            {
+                _userRepository.AddItem(data);
+                _userRepository.UnitOfWork.CommitInt();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                response.Message = e.InnerException != null ? e.InnerException.Message : e.Message;
+            }
+
+            return response;
+
         }
     }
 }
